2ddf56d4f7875a6f5f71e2774a0d62fe3815328a
Ganesh-Mali-23 <ganeshmali3844@gmail.com>
2026-09-11 12:10:25 +0530

    Added Routes

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
 .then(() => {
-    console.log("Database connected");
+    console.log("Database connected"); 
 })
 .catch((err) => {
     console.log(err);
@@ -17,7 +23,47 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 };
 
+app.get("/listings" , async (req,res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs" , {allListings});
+});
 
+app.get("/listings/new" , (req,res) => {
+    res.render("listings/new.ejs");
+});
+
+app.get("/listings/:id" , async (req,res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs" , {listing});
+
+});
+
+app.get("/listings/:id/edit" , async (req,res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs" , {listing});
+
+});
+
+app.put("/listings/:id" , async(req,res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id , {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+app.post("/listings/new" , async(req,res) => {
+    console.log(req.body);
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+app.delete("/listings/:id" , async(req,res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+});
 
 app.listen(3000 , () => {
     console.log("Server Started");
